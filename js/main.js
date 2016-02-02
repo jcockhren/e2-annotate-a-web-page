4e4626cab578736069d72bd3910429f4aab4cd0b
Joel Harris <joelharris229@gmail.com>
2016-02-01 20:55:42 -0600

    code can search paragraphs and find if there's a match related to input box

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-console.log("hello world");
-
 $('#button').on('click', function(){
   var value = $('#inputBox').val();
  
   $('p').each(function(index){
     var hello = $(this).text();
     console.log(hello);
+    var x = hello.search(value);
+    console.log(x);
+    if(x !== -1 ){
+      console.log("results found");
+    }else{
+      console.log("no results found");
+    }
+
+
   })
 
 })
