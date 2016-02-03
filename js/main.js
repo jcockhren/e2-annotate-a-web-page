a96e586caac367f8b5f2ae6333e44f7ed7f35337
Martin Cross <mncross@gmail.com>
2016-02-03 13:14:01 -0600

    add basic logging of selection on mouseup

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $('#button').on('click', function(){
   var value = $('#inputBox').val();
   var valLength = value.length;
   console.log(valLength);
- 
+
   $('p').each(function(index){
     var hello = $(this).text();
     var x = hello.search(value);
@@ -14,4 +14,12 @@ $('#button').on('click', function(){
       console.log("no results found");
     }
   })
+})
+
+
+$(document).on('mouseup', function(){
+  var selection = document.getSelection();
+  console.log(selection);
+  var selectedText = selection.toString();
+  console.log(selectedText);
 })
