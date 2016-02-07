00c6698c4d6a73e455d6f7c475c914cb612664ef
Brian Merchant <btmerchant@comcast.net>
2016-02-07 16:52:11 -0600

    WikiCall now getting synopsys response and put it in an overlayed window

--- js/wikiCall.js
+++ js/wikiCall.js
@@ -1,16 +1,17 @@
 window.onmouseup = function() {
   var data = getSelectionText();
-  var str1 = "http://en.wikipedia.org/w/api.php?action=parse&page=";
+  var str1 = "http://en.wikipedia.org/w/api.php?action=parse&section=0&page=";
   var str2 = str1.concat(data);
-  var callStr = str2.concat("&prop=text&format=json&callback=?");
+  var callStr = str2.concat("&rvsection=0&prop=text&format=json&callback=?");
   console.log("MouseUp");
   console.log("Text = ",data);
+  $( "#view" ).append("<div class='wikiWindow' id='wikiInfo'>&nbsp</div>");
 
-   $.getJSON(callStr, function(json) {
+
+  var wiki =  $.getJSON(callStr, function(json) {
     $('#wikiInfo').html(json.parse.text['*']);
     $("#wikiInfo").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");});
     $("#wikiInfo").find("a").attr("target", "_blank");
   });
-};
-
-
+  // console.log("Wiki= ",wiki);
+  };
