deee1102fca85f24dd2822dbbf51f621b8671e83
Joel Harris <joelharris229@gmail.com>
2016-02-08 19:45:02 -0600

    reinstantiated highlight in with brain's new getselection function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 function getSelectionText() {
     var self = this;
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
+    if (document.getSelection) {
+        var text = document.getSelection()
+        var textsel = text.toString();
+        console.log(textsel);
+        var selectionNode = text.anchorNode.parentElement.innerHTML;
+        var markedText = selectionNode.replace(textsel, "<mark>"+textsel+"</mark>");
+        $('.paragraph1').html(markedText);
     } else if (document.selection && document.selection.type != "Control") {
         text = document.selection.createRange().text;
     }
     console.log("Selection Text = ",text);
     return text;
 }
-// getSelectionText();
+getSelectionText();
 $(function () {
   $('[data-toggle="popover"]').popover();
 });
@@ -36,6 +40,7 @@ $('#button').popover();
 // $(document).on('mouseup', function(){
 //   var selection = document.getSelection();
 //   var selectedText = selection.toString();
+//   console.log(selectedText);
 //   var selectionNode = selection.anchorNode.parentElement.innerHTML;
 //   var markedText = selectionNode.replace(selectedText, "<mark>"+selectedText+"</mark>");
 //   $('.paragraph1').html(markedText);
