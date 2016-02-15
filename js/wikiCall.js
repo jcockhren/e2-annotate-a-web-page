bcff0914b281a3c0a13332fefa724a4ddb05d892
Joel Harris <joelharris229@gmail.com>
2016-02-15 11:31:37 -0600

    finished styling main page and side bar, added elements to sidebar

--- js/wikiCall.js
+++ js/wikiCall.js
@@ -3,15 +3,18 @@ $('article').mouseup (function() {
   var wikiApiString = "http://en.wikipedia.org/w/api.php?action=parse&section=0&page=" + data + "&rvsection=0&prop=text&format=json&callback=?";
   // var WikiPlusDataString = str1.concat(data);
   // var callStr = str2.concat("&rvsection=0&prop=text&format=json&callback=?");
-  $( "#view" ).append("<div class='wikiWindow' id='wikiInfo'>&nbsp</div>");
+  // $( "#view" ).append("<div class='wikiWindow' id='wikiInfo'>&nbsp</div>");
+  $("#selection").text(data);
+  $("#wikiInfo").removeClass("displayNone");
     var wiki =  $.getJSON(wikiApiString, function(json) {
       $("."+data+"").attr('data-content', json.parse.text['*']);
-
-      $('#wikiInfo').html(json.parse.text['*']);
-      $("#wikiInfo").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");});
-      $("#wikiInfo").find("a").attr("target", "_blank");
+      console.log(json)
+      // $('#wikiInfo').html(json.parse.text['*']);
+      // $("#wikiInfo").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");});
+      // $("#wikiInfo").find("a").attr("target", "_blank");
     });
 });
-$("#view").click(function() {
-  $("#view").empty();
+$(".glyphicon-remove-circle").click(function() {
+  $("#wikiInfo").addClass("displayNone");
+  $("#selection").empty();
 });
